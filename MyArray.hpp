72096471d04984a71493c1161e611bb62c329f7e
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2022-09-12 15:42:53 +0530

    head myArray.hpp

---
+++ MyArray.hpp
@@ -0,0 +1,9 @@
+#ifndef __MY_ARRAY__
+#define __MY_ARRAY__
+template<typename Type>
+class MyArray
+{
+private:
+public:
+};
+#endif//MyArray.hpp
---
+++ examples/main.cc
@@ -0,0 +1,5 @@
+#include"../MyArray.hpp"
+int main()
+{
+  return 0;
+};
